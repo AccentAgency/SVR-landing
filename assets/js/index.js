85b1cbf7e386f6abe1fc5d6c1f387b6362149dee
AccentAgency <accentagencyweb@gmail.com>
2021-06-17 16:50:42 -0400

    jndj

--- assets/js/index.js
+++ assets/js/index.js
@@ -109,7 +109,7 @@
         },
         zoom: {
         enabled: true, // By default it's false, so don't forget to enable it
-
+        autoplay: true,
         duration: 300, // duration of the effect, in milliseconds
         easing: 'ease-in-out', // CSS transition easing function
 
