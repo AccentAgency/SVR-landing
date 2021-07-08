fea89bd86571d374f5fdc44d63d54f972f9a69b9
AccentAgency <accentagencyweb@gmail.com>
2021-07-08 11:55:40 -0400

    load

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,57 +4,67 @@
 
     // PRE LOADER
 
-    setTimeout(function(){ $('.preloader').fadeOut(1000); }, 1000);
 
+    $('.owl-carousel').on('initialized.owl.carousel', function(event){
+      $('.preloader').fadeOut(1000);
+    })
+
+    $('.owl-carousel').owlCarousel({
+      animateOut: 'fadeOut',
+      navigation : false,
+      items:1,
+      autoplay:true,
+      smartSpeed:1000
+    })
 
     document.addEventListener("DOMContentLoaded", function() {
-  var lazyloadImages;
+    var lazyloadImages;
 
-  if ("IntersectionObserver" in window) {
-    lazyloadImages = document.querySelectorAll(".lazy");
-    var imageObserver = new IntersectionObserver(function(entries, observer) {
-      entries.forEach(function(entry) {
-        if (entry.isIntersecting) {
-          var image = entry.target;
-          image.classList.remove("lazy");
-          imageObserver.unobserve(image);
-        }
+    if ("IntersectionObserver" in window) {
+      lazyloadImages = document.querySelectorAll(".lazy");
+      var imageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            var image = entry.target;
+            image.classList.remove("lazy");
+            imageObserver.unobserve(image);
+          }
+        });
       });
-    });
 
-    lazyloadImages.forEach(function(image) {
-      imageObserver.observe(image);
-    });
-  } else {
-    var lazyloadThrottleTimeout;
-    lazyloadImages = document.querySelectorAll(".lazy");
+      lazyloadImages.forEach(function(image) {
+        imageObserver.observe(image);
+      });
+    } else {
+      var lazyloadThrottleTimeout;
+      lazyloadImages = document.querySelectorAll(".lazy");
 
-    function lazyload () {
-      if(lazyloadThrottleTimeout) {
-        clearTimeout(lazyloadThrottleTimeout);
+      function lazyload () {
+        if(lazyloadThrottleTimeout) {
+          clearTimeout(lazyloadThrottleTimeout);
+        }
+
+        lazyloadThrottleTimeout = setTimeout(function() {
+          var scrollTop = window.pageYOffset;
+          lazyloadImages.forEach(function(img) {
+              if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+              }
+          });
+          if(lazyloadImages.length == 0) {
+            document.removeEventListener("scroll", lazyload);
+            window.removeEventListener("resize", lazyload);
+            window.removeEventListener("orientationChange", lazyload);
+          }
+        }, 20);
       }
 
-      lazyloadThrottleTimeout = setTimeout(function() {
-        var scrollTop = window.pageYOffset;
-        lazyloadImages.forEach(function(img) {
-            if(img.offsetTop < (window.innerHeight + scrollTop)) {
-              img.src = img.dataset.src;
-              img.classList.remove('lazy');
-            }
-        });
-        if(lazyloadImages.length == 0) {
-          document.removeEventListener("scroll", lazyload);
-          window.removeEventListener("resize", lazyload);
-          window.removeEventListener("orientationChange", lazyload);
-        }
-      }, 20);
+      document.addEventListener("scroll", lazyload);
+      window.addEventListener("resize", lazyload);
+      window.addEventListener("orientationChange", lazyload);
     }
-
-    document.addEventListener("scroll", lazyload);
-    window.addEventListener("resize", lazyload);
-    window.addEventListener("orientationChange", lazyload);
-  }
-})
+  })
 
 
     // MENU
@@ -80,15 +90,6 @@
       })
     });
 
-    $('.owl-carousel').owlCarousel({
-      animateOut: 'fadeOut',
-      navigation : false,
-      items:1,
-      loop:true,
-      lazyLoad:true,
-      autoplay:true,
-      smartSpeed:1000
-    })
 
     document.getElementsByTagName("html")[0].style.overflow = "hidden";
     document.getElementsByTagName("html")[0].style.overflow = "auto";
